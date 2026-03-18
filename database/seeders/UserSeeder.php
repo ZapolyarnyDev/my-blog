44e58a8454c882d12b95925b2ebfa23aa7a84fd7
Zapolyarny <zapolyarnynorth@gmail.com>
2026-03-18 15:14:14 +0300

    chore(seeder): add default regular user credentials

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,6 +9,14 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        User::query()->firstOrCreate(
+            ['email' => 'user@example.com'],
+            [
+                'name' => 'User',
+                'password' => 'password',
+            ]
+        );
+
         User::factory()->count(10)->create();
     }
 }
